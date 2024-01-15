638ba5454a771d98cb19ea6dad529ce44f226cf4
Vivian Igboasoiyi <vivian.igbo4mails@gmail.com>
2024-01-15 17:27:22 +0100

    player in myworld eingefügt

--- zombiesLB1/Player.java
+++ zombiesLB1/Player.java
@@ -12,6 +12,12 @@ public class Player extends Actor
      * Act - do whatever the Player wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    
+    public Player () {
+        setImage(new GreenfootImage(50,50));
+        getImage().setColor(Color.YELLOW); //farbe des spielers setzen
+        getImage().fillOval(0, 0, 50, 50); // form des spielers setzen
+    }
     public void act()
     {
         // Add your action code here.
