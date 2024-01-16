65db89e3dc5f628272805783b1d27a5cb75517e7
Vivian Igboasoiyi <vivian.igbo4mails@gmail.com>
2024-01-16 17:16:06 +0100

    zombie ist walking

--- zombiesLB1/MyWorld.java
+++ zombiesLB1/MyWorld.java
@@ -16,7 +16,7 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(700, 600, 1); 
+        super(1000, 800, 1); 
         addObject(new Player(), getWidth()/2, getHeight()/2); //player in myWorld einfügen
     }
 }
--- zombiesLB1/Zombie.java
+++ zombiesLB1/Zombie.java
@@ -12,8 +12,27 @@ public class Zombie extends Actor
      * Act - do whatever the Zombie wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    int animateImage = 0;
+    int count;
+    
+    public Zombie() {
+        setImage("skeleton-idle_16.png");
+        getImage().scale(80,80);
+    }
     public void act()
     {
-        // Add your action code here.
+        count++;
+        animate();
+    }
+    
+    public void animate() {
+        if(count % 8 == 0) {
+            if(animateImage > 16) {
+                animateImage = 0;
+            }
+            setImage("skeleton-move_" + animateImage + ".png"); //zombies laufen
+            animateImage++;
+            
+        }
     }
 }
